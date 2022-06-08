968d0606d465d2f673eafdf92d9cd833b024aed8
FuJr <for360anquanzhongxin@outlook.com>
2022-06-08 20:11:16 +0800

    add response info

--- message/plugins/base_utility.py
+++ message/plugins/base_utility.py
@@ -25,7 +25,7 @@ class base_utility:
                 'message' : message
             }
         self.api_queue.put([send_api,post_data])
-        return self.api_res_queue.get() 
+        return json.loads(self.api_res_queue.get())
     
 
     def add_log(self,log_level:int,log_msg):
