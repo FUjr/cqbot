9c1bd83247d028cf78f9ddacf6b9bbf957004227
FuJr <for360anquanzhongxin@outlook.com>
2022-06-09 15:27:01 +0800

    bug fixed

--- message/plugins/plugin_timer.py
+++ message/plugins/plugin_timer.py
@@ -30,14 +30,17 @@ class plugin_timer(base_utility.base_utility):
     
     def parse_time(self,time_arg):
         time_arg = time_arg.replace('：',':').replace('点',":").replace('分','')
-        local_time = time.localtime(time.time())
+        m_year = time.strftime('%Y',time.localtime())
+        m_mon = time.strftime('%m',time.localtime())
+        m_mday = time.strftime('%d',time.localtime())
         if '.' in time_arg:
             if len(time_arg.split('.')[0].split('-')) == 2:
-                timestruct = '%m-%d.%H:%M'
+                time_arg =  m_year + '-' + time_arg
             elif len(time_arg.split('.')[0].split('-')) == 3:
-                timestruct = '%m-%d.%H:%M'
-                time_struct = time.strptime(time_arg,timestruct)
-                time_struct.tm_year = local_time.tm_year
+                pass
+            else:
+                return False
+                
         elif '.' not in time_arg:
             rex = r'(\d{1,})'
             if not re.match(rex,time_arg):
@@ -47,27 +50,30 @@ class plugin_timer(base_utility.base_utility):
                 if len(time_list) != 1:
                     return False
                 return time_list[0]
-            elif 'm' in time_list:
+            elif 'm' in time_arg:
                 time_list = re.findall(rex,time_arg)
                 if len(time_list) != 1:
                     return False
                 return time_list[0]*60
-            elif 'h' in time_list:
+            elif 'h' in time_arg:
                 time_list = re.findall(rex,time_arg)
                 if len(time_list) != 1:
                     return False
                 return time_list[0]*60*60
             elif ':' in time_arg:
-                timestruct = '%H:%M'
-                time_struct = time.strptime(time_arg,timestruct)
-                time_struct.tm_year = local_time.tm_year
-                time_struct.tm_mon = local_time.tm_mon
-                time_struct.tm_mday = local_time.tm_mday
+                time_arg = m_year + '-' + m_mon + '-' + m_mday + '.' + time_arg 
+
+
+                
                 
         else:
             return False
-        target_time = time.mktime(time_struct)
-        gap_time = time.time() - target_time
+        
+        target_time = time.strptime(time_arg,"%Y-%m-%d.%H:%M")
+        target_time = time.mktime(target_time)
+        print(time_arg)
+        gap_time = target_time - time.time()
+        print(gap_time)
         if gap_time < 0:
             return False
         else:
@@ -76,18 +82,22 @@ class plugin_timer(base_utility.base_utility):
     async def delay_callback(self,delay,function,*args,**kwargs):
         await asyncio.sleep(delay)
         function(*args,**kwargs)
-     
+    
+    def run(self,data):
+        self.main()
+        return False
     def main(self):
         support_form = '[19：00] [20:42] [19点29分] [6-10.18:20] [2022-6-9.18:15] [3s/m/h]'
         cmd_format = '/定时消息 定时 消息'
         require_cmd_sum = 3
         args = self.cmd_parse(require_cmd_sum,cmd_format)
         try:
-            wait_time = self.parse_time(args[1])
-            if isinstance(wait_time,float) or isinstance(wait_time,int):
-                asyncio.create_task(self.delay_callback(args[0],self.send_back_msg,args[2]))
-                self.send_back_msg('已设置定时%s秒 后发送%s' %(wait_time,args[1]))
+            wait_time = int(self.parse_time(args[1]))
+            if isinstance(wait_time,int):
+                asyncio.create_task(self.delay_callback(wait_time,self.send_back_msg,args[2]))
+                self.send_back_msg('已设置定时%s秒 后发送%s' %(wait_time,args[2]))
             else:
                 self.send_back_msg('请输入未来的时间或者正确的格式 %s' %support_form)
-        except:
+        except Exception as e:
+            self.add_log(5,e.args)
             self.send_back_msg('请输入正确的格式 %s' %support_form)
