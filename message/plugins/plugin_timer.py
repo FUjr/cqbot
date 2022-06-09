ad6ad779997a15ca542e8e93eefc7d7734c7cc83
FuJr <for360anquanzhongxin@outlook.com>
2022-06-09 13:38:16 +0800

    add echo imform

--- message/plugins/plugin_timer.py
+++ message/plugins/plugin_timer.py
@@ -84,8 +84,9 @@ class plugin_timer(base_utility.base_utility):
         args = self.cmd_parse(require_cmd_sum,cmd_format)
         try:
             wait_time = self.parse_time(args[1])
-            if isinstance(wait_time,float):
+            if isinstance(wait_time,float) or isinstance(wait_time,int):
                 asyncio.create_task(self.delay_callback(args[0],self.send_back_msg,args[2]))
+                self.send_back_msg('已设置定时%s秒 后发送%s' %(wait_time,args[1])
             else:
                 self.send_back_msg('请输入未来的时间或者正确的格式 %s' %support_form)
         except:
