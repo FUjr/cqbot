7c906e406520a5101c1adc08141062186955146d
FuJr <for360anquanzhongxin@outlook.com>
2022-07-10 23:57:02 +0800

    bug fix

--- message/plugins/plugin_fakefresh.py
+++ message/plugins/plugin_fakefresh.py
@@ -58,7 +58,6 @@ class plugin_fakefresh(base_utility.base_utility):
         res = self.query_api('get_group_member_list',{'group_id':group_id})
         for i in res['data']:
             if str(i['user_id']) == qq:
-                self.send_back_msg('%s还在%s群里' % (self.at_info,group_id))
                 same_people_buffer += '还在%s群里\n' % i['group_name']
         return same_people_buffer
     
