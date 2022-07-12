38b2a0ce3de0050d2f633e0496ff551e8e53e295
FuJr <for360anquanzhongxin@outlook.com>
2022-07-12 10:52:53 +0800

    add new function to check all groups`

--- message/plugins/plugin_fakefresh.py
+++ message/plugins/plugin_fakefresh.py
@@ -1,5 +1,6 @@
 import time
 import re
+from tokenize import group
 from . import base_utility
 alia = ['真假新生']
 help = {
@@ -14,16 +15,26 @@ permission = {
     'role' : 'member'
 }
 class plugin_fakefresh(base_utility.base_utility):
+    
+    
     def run(self,data):
+        self.cache_data = {}
         at_info = self.first_message['message']
         rex = r'qq=(\d{1,})'
         check_qq = re.findall(rex,at_info)
+        if 'all' in self.first_message['message']:
+            self.check_all()
+            return False
         if len(check_qq) == 0:
             self.send_back_msg('请发送 /真假新生 @要查的人')
             return False
         else :
             for i in check_qq:
-                self.get_fakefresh_info(i)
+                res = self.get_fakefresh_info(i)
+                if len(res) > 0:
+                    self.send_back_msg(self.get_at_info(i) + '进入本群的时间为' + self.attend_group_time() + '\n还在这些群里：' + '\n'.join(res))
+                else:
+                    self.send_back_msg(self.get_at_info(i) + '还没有进入其他群.\n ' + '进入本群的时间为' + self.attend_group_time() )
             return False
         
     def get_fakefresh_info(self,qq) -> False:
@@ -34,21 +45,14 @@ class plugin_fakefresh(base_utility.base_utility):
             self.send_back_msg('这么可爱的汤姆，怎么可能不是新生呢')
             return False
 
-        same_people_buffer = ''
+        same_group = []
         group_list,group_name_list = self.get_group_list()
         for i in group_list:
             group_name = group_name_list[group_list.index(i)]
-            res = self.get_same_people(i,group_name,qq)
-            if res != '':
-                same_people_buffer += res
-                same_people_buffer += '\n'
-        if same_people_buffer == '':
-            self.send_back_msg(self.attend_group_time(qq))
-            return False
-        else:
-            self.at_info = self.get_at_info(qq)
-            self.send_back_msg(self.at_info + '\n' + self.attend_group_time(qq) + '\n' + same_people_buffer)
-            return False
+            res = self.if_in_other_group(i,qq)
+            if (res):
+                same_group.append(group_name)
+        return same_group
     
     def get_at_info(self,qq) -> str:
         at_info = '[CQ:at,qq=%s]' % qq
@@ -63,17 +67,50 @@ class plugin_fakefresh(base_utility.base_utility):
             group_name.append(i['group_name'])
         return group_list,group_name
     
-    def get_same_people(self,group_id,group_name,qq) -> str:
-        same_people_buffer = ''
-        res = self.query_api('get_group_member_list',{'group_id':group_id})
+    def if_in_other_group(self,group_id,qq) -> str:
+        in_other = False
+        if self.cache_data.get(group_id) == None:
+            res = self.query_api('get_group_member_list',{'group_id':group_id})
+            self.cache_data[group_id] = res
+        else:
+            res = self.cache_data[group_id]
+        
         for i in res['data']:
-            if str(i['user_id']) == qq:
-                same_people_buffer += '还在 %s  群里\n' % (group_name)
-        return same_people_buffer
+            if str(i['user_id']) == str(qq):
+                in_other += True
+        return in_other
     
     def attend_group_time(self,qq) -> str:
         res = self.query_api('get_group_member_info',{'group_id':self.first_message['group_id'],'user_id':qq})
         timestamp =   res['data']['join_time']
         dt = time.localtime(timestamp)
-        print(dt)
         return '进本群时间是%s' % time.strftime("%Y-%m-%d %H:%M:%S",dt)
+    
+    def check_all(self):
+        get_user_id_api = 'get_group_member_list'
+        user_id_list = self.query_api(get_user_id_api,{'group_id':self.first_message['group_id']})
+        group_list,group_name_list = self.get_group_list()
+        report_dict = {}
+        count = 0
+        for i in group_name_list:
+            report_dict[i] = 0
+        for i in user_id_list['data']:
+            if str(i['user_id']) == str(self.first_message['self_id']):
+                continue
+            if str(i['user_id']) == '1194436766':
+                continue
+            if str(i['user_id']) == '2854196310':
+                continue
+            in_other = False
+            for j in group_list:
+                if j == self.first_message['group_id']:
+                    continue
+                res = self.if_in_other_group(j,i['user_id'])
+                if (res):
+                    in_other = True
+                    report_dict[group_name_list[group_list.index(j)]] += 1
+            if in_other:
+                count += 1
+        none_zero = '\n'.join([i + ':' + str(report_dict[i]) for i in report_dict if report_dict[i] > 0])
+        report = '本群共有%s人进入了其他群，各群人数为\n%s' % (str(count),none_zero)
+        self.send_back_msg(report)
