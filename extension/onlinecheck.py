a75c4c986162360d5824e30984440089d9ddfb97
FuJr <for360anquanzhongxin@outlook.com>
2022-07-15 22:35:39 +0800

    add error handler

--- extension/onlinecheck.py
+++ extension/onlinecheck.py
@@ -23,37 +23,40 @@ def http_server(lock):
         send = 'HTTP/1.1 200 OK\r\n'
         send += 'Content-Type:image/jpeg\r\n\r\n'
         while True:
-            conn , addr = s.accept()
-            #接收客户端发送的数据
-            data = conn.recv(1024)
-            #编码客户端数据
-            data = data.decode('utf-8')
-            print(data)
-            x_forward_re = r'X-Forwarded-For:(.*?)\r\n'
-            path_re = r'GET (.*?) HTTP/1.1\r\n'
-            ua_re = r'User-Agent:(.*?)\r\n'
-            x_forward = re.findall(x_forward_re,data)
-            path = re.findall(path_re,data)
-            ua = re.findall(ua_re,data)
-            conn.send(send.encode('utf-8'))
-            conn.send(send_data)
-            conn.close()
-            if len(x_forward) > 0:
-                x_forward = x_forward[0]
-            else:
+            try:
+                conn , addr = s.accept()
+                #接收客户端发送的数据
+                data = conn.recv(1024)
+                #编码客户端数据
+                data = data.decode('utf-8')
+                print(data)
+                x_forward_re = r'X-Forwarded-For:(.*?)\r\n'
+                path_re = r'GET (.*?) HTTP/1.1\r\n'
+                ua_re = r'User-Agent:(.*?)\r\n'
+                x_forward = re.findall(x_forward_re,data)
+                path = re.findall(path_re,data)
+                ua = re.findall(ua_re,data)
+                conn.send(send.encode('utf-8'))
+                conn.send(send_data)
+                conn.close()
+                if len(x_forward) > 0:
+                    x_forward = x_forward[0]
+                else:
+                    continue
+                if len(path) > 0:
+                    path = path[0]
+                else:
+                    continue
+                if len(ua) > 0:
+                    ua = ua[0]
+                else:
+                    ua = 'unknown'
+                report = {'ip':x_forward.replace(' ',''),'path':path,'ua':ua,'time':time.time()}
+                lock.acquire()
+                get_data.append(report)
+                lock.release()
+            except:
                 continue
-            if len(path) > 0:
-                path = path[0]
-            else:
-                continue
-            if len(ua) > 0:
-                ua = ua[0]
-            else:
-                ua = 'unknown'
-            report = {'ip':x_forward.replace(' ',''),'path':path,'ua':ua,'time':time.time()}
-            lock.acquire()
-            get_data.append(report)
-            lock.release()
 
 
 def return_data(lock):
@@ -70,42 +73,44 @@ def return_data(lock):
     s.listen(20)
     send = 'HTTP/1.1 200 OK\r\nContent-Type:plain/text\r\n\r\n'
     while True:
-        conn , addr = s.accept()
-        #接收客户端发送的数据
-        data = conn.recv(1024)
-        #编码客户端数据
-        data = data.decode('utf-8')
-        print(data)
-        path_re = r'GET (.*?) HTTP/1.1\r\n'
-        
-        path_re = re.findall(path_re,data)
-        
-        if len(path_re) > 0:
-            path_re = path_re[0]
-            print(path_re)
-            buffer = []
-            print(get_data)
-            for i in get_data:
-                print(i)
-                if i['path'] == path_re:
-                    buffer.append(i)
-                    lock.acquire()
-                    get_data.remove(i)
-                    lock.release()
-                    continue
-                if time.time() -  i['time'] > 30:
-                    lock.acquire()
-                    get_data.remove(i)
-                    lock.release()
-            json_data = json.dumps(buffer)
-            conn.send(send.encode('utf-8'))
-            conn.send(json_data.encode('utf-8'))
-            conn.close()
-        else:
-            send = 'HTTP/1.1 404 Not Fount\r\nContent-Type:plain/text\r\n\r\n'
-            conn.send(send.encode('utf-8'))
-            conn.close()
+        try:
+            conn , addr = s.accept()
+            #接收客户端发送的数据
+            data = conn.recv(1024)
+            #编码客户端数据
+            data = data.decode('utf-8')
+            print(data)
+            path_re = r'GET (.*?) HTTP/1.1\r\n'
+            
+            path_re = re.findall(path_re,data)
+            
+            if len(path_re) > 0:
+                path_re = path_re[0]
+                print(path_re)
+                buffer = []
+                print(get_data)
+                for i in get_data:
+                    print(i)
+                    if i['path'] == path_re:
+                        buffer.append(i)
+                        lock.acquire()
+                        get_data.remove(i)
+                        lock.release()
+                        continue
+                    if time.time() -  i['time'] > 30:
+                        lock.acquire()
+                        get_data.remove(i)
+                        lock.release()
+                json_data = json.dumps(buffer)
+                conn.send(send.encode('utf-8'))
+                conn.send(json_data.encode('utf-8'))
+                conn.close()
+            else:
+                send = 'HTTP/1.1 404 Not Fount\r\nContent-Type:plain/text\r\n\r\n'
+                conn.send(send.encode('utf-8'))
+                conn.close()
+                continue
+        except:
             continue
-        
         
 
