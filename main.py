ccba84e08e63a5c8a00b1429c2889e3ec90a4a9f
FuJr <for360anquanzhongxin@outlook.com>
2022-07-15 21:05:17 +0800

    remove http server from main.py

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ import time
 import sys
 import notice
 import message
-import socket
+from extension import onlinecheck
 import request
-import re
 
 ws = 'ws://192.168.3.5:6700'
 http_get = 'http://192.168.3.5:678/'
@@ -60,54 +59,6 @@ def log_thread( log_queue :Queue):
         if log[0] > log_level:
             print(log[1])
 
-
-def http_server(lock):
-        global user_data
-        #创建socket对象
-        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        #创建一个简易http服务器线程，通过管道通信
-        #设置监听端口和地址
-        port = 8080
-        addr = '0.0.0.0'
-        #绑定端口
-        s.bind((addr,port))
-        
-        #监听端口
-        s.listen(20)
-        
-        with open ('mypic.jpg','rb') as f:
-            send_data = f.read()
-        send = 'HTTP/1.1 200 OK\r\n'
-        send += 'Content-Type:image/jpeg\r\n\r\n'
-        while True:
-            conn , addr = s.accept()
-            #接收客户端发送的数据
-            data = conn.recv(1024)
-            #编码客户端数据
-            data = data.decode('utf-8')
-            x_forward_re = r'X-Forwarded-For:(.*?)\r\n'
-            path_re = r'GET (.*?) HTTP/1.1\r\n'
-            ua_re = r'User-Agent:(.*?)\r\n'
-            x_forward = re.findall(x_forward_re,data)
-            path = re.findall(path_re,data)
-            ua = re.findall(ua_re,data)
-            conn.send(send.encode('utf-8'))
-            conn.send(send_data)
-            conn.close()
-            if len(x_forward) > 0:
-                x_forward = x_forward[0]
-            else:
-                continue
-            if len(path) > 0:
-                path = path[0]
-            else:
-                continue
-            if len(ua) > 0:
-                ua = ua[0]
-            else:
-                ua = 'unknown'
-            report = {'ip':x_forward.replace(' ',''),'path':path,'ua':ua,'time':time.time()}
-            user_data.append(report)
 user_data = []
 lock = threading.Lock()
 log_queue = Queue()
@@ -121,8 +72,6 @@ query_api = threading.Thread(target=query_api,args=(api_queue,api_res_queue))
 query_api.start() 
 print_log = threading.Thread(target=log_thread,args=(log_queue,))
 print_log.start()
-http_server = threading.Thread(target=http_server,args=(lock,))
-http_server.start()
 asyncio.run(getmessage()) 
 
 
